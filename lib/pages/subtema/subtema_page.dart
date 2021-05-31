76d2205015ac7c9d7e11aa10c954631881dbfc13
Daniel Marques <daniel.brz2009@gmail.com>
2021-05-31 17:55:35 -0300

    Tema and Subtema Screens

---
+++ lib/pages/subtema/subtema_page.dart
@@ -0,0 +1,60 @@
+import 'package:CoolCall/core/app_gradients.dart';
+import 'package:CoolCall/pages/download/download_page.dart';
+import 'package:CoolCall/shared/button_widget.dart';
+import 'package:CoolCall/shared/card_widget.dart';
+import 'package:flutter/material.dart';
+
+class SubtemaPage extends StatefulWidget {
+  @override
+  _SubtemaPageState createState() => _SubtemaPageState();
+}
+
+class _SubtemaPageState extends State<SubtemaPage> {
+  final String text = "Subtema";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(gradient: AppGradients.linear),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 75),
+            child: Column(
+              children: [
+                Center(
+                    child: Text(
+                  "Subtemas",
+                  style: TextStyle(
+                      fontSize: 27.5,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                )),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 30, top: 40),
+                  child: Container(height: 130, child: CardWidget(this.text)),
+                ),
+                GridView.count(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,
+                  crossAxisCount: 2,
+                  children: [
+                    CardWidget(this.text),
+                    CardWidget(this.text),
+                    CardWidget(this.text),
+                    CardWidget(this.text)
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: ButtonWidget(DownloadPage()),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
